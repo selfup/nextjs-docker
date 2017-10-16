20850371dad4cf3320fc728bd52fd72bbec08972
Regis <boudinot.regis@yahoo.com>
2017-10-16 09:47:50 -0500

    setup appropriate benchmark for next after prod build in 'prod.sh' container build

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,6 +2,4 @@ ab \
   -n 100000 \
   -c 50 \
   -k -v 1 \
-  -H "Accept-Encoding: gzip, deflate" \
-  -T "application/json" \
-  http://localhost:8080/ > .results \
+  http://localhost:3000/ > .results \
